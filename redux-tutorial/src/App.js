c485975bfd77bf9f305e686414cf9ac748978dad
AlexanderBowers <7319abowers@gmail.com>
2021-08-21 15:30:32 -0700

    added dispatch and defined increment action

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment} from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter)
   const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+      <button>+</button>
       {isLogged ? <h3>This variable is false so I won't see this</h3> : ''}
     </div>
   );
