ac34496cd28318eee04f5c7595d074f8a993ef5d
AlexanderBowers <7319abowers@gmail.com>
2021-08-21 15:33:31 -0700

    added decrement

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useSelector, useDispatch} from 'react-redux';
-import {increment} from './actions';
+import {increment, decrement} from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter)
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <h1>Counter {counter}</h1>
       <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? <h3>This variable is false so I won't see this</h3> : ''}
     </div>
   );
