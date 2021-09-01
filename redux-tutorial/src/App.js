f537582caf97bb8af3dbf8f27bf443f696d3ad08
AlexanderBowers <7319abowers@gmail.com>
2021-08-31 17:24:33 -0700

    added react-redux-form

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useSelector, useDispatch} from 'react-redux';
 import {increment, decrement} from './actions';
+import {LocalForm, Control} from 'react-redux-form'
 
 function App() {
   const counter = useSelector(state => state.counter)
@@ -8,15 +9,24 @@ function App() {
   const input = useSelector(state => state.input)
   const dispatch = useDispatch()
 
+  handleChange(values) 
+  handleUpdate(form) 
+  handleSubmit(values) 
+
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? <h3>This variable is false so I won't see this</h3> : ''}
-      <form onSubmit={() => dispatch(input())} >
-        <input type="text"></input>
-      </form>
+
+      <LocalForm
+        onUpdate={(form) => this.handleUpdate(form)}
+        onChange={(values) => this.handleChange(values)}
+        onSubmit={(values) => this.handleSubmit(values)}
+      >
+        <Control.text model="testForm"/>
+      </LocalForm>
     </div>
   );
 }
