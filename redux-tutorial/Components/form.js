f135ff7a5a201849a75baddc84aced2e7087c93c
AlexanderBowers <7319abowers@gmail.com>
2021-09-11 17:51:09 -0700

    added first name form

--- redux-tutorial/Components/form.js
+++ redux-tutorial/Components/form.js
@@ -3,7 +3,13 @@ import {Field, reduxForm} from 'redux-form'
 
 let ContactForm = props => {
     const {handleSubmit} = props
-    return <form onSubmit={handleSubmit}>{/* form body*/}</form>
+    return (
+        <form onSubmit={handleSubmit}>
+       <div>
+           <label htmFor='firstName' component="input" type="text" />
+       </div>
+       </form>
+    )
 }
 
 ContactForm = reduxForm({
