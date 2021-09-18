080927d49056ac98aaf5cd471d29c27cbf6ceaad
AlexanderBowers <7319abowers@gmail.com>
2021-09-18 14:39:53 -0700

    adding newForm to diagnose

---
+++ redux-tutorial/src/Components/newForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const newForm = (props) => {
+    return <button>press me!</button>
+}
+
+export default newForm
--- redux-tutorial/src/containers/formPage.js
+++ redux-tutorial/src/containers/formPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {ContactForm} from './components/form'
+//import {ContactForm} from './components/form'
+import {newForm} from './components/newForm'
 
 export const FormPage = (props) => {
     submit = values => {
         console.log(values)
     }
     render() 
-        return <ContactForm onSubmit={this.submit}/>
+        //return <ContactForm onSubmit={this.submit}/>
+        return <newForm/>
     
 }
 export default FormPage
